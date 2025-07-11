b8cadf3680b293155b8ae42533c4604dd0644048
Misael Lopes <mecl.ely@gmail.com>
2025-07-11 16:38:43 +0100

    feat: listing rooms

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -2,8 +2,18 @@ import { reset, seed } from 'drizzle-seed'
 import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
 
-await reset(db, { schema })
+await reset(db, schema)
 
-await seed(db, { schema })
+await seed(db, schema).refine((faker) => {
+  return {
+    rooms: {
+      count: 24,
+      columns: {
+        name: faker.companyName(),
+        description: faker.loremIpsum(),
+      },
+    },
+  }
+})
 
 await sql.end()
